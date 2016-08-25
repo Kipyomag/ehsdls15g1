2b06a48a1cee8339ee44d287ce9eb6adc54d36ba
LaGvidilo <cyrilcoelho67@gmail.com>
2016-08-25 11:57:26 +0200

    Génération du CRUD

---
+++ src/EhsBundle/Form/ArticlesType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace EhsBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ArticlesType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('titre')
+            ->add('contenu')
+            ->add('chapeau')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'EhsBundle\Entity\Articles'
+        ));
+    }
+}
