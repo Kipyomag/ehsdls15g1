56954177249e7d684b7c4d3f17e3213bd0687e06
LaGvidilo <cyrilcoelho67@gmail.com>
2016-08-25 11:48:55 +0200

    Ajout de l'entité Articles

---
+++ src/EhsBundle/Entity/Articles.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace EhsBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Articles
+ *
+ * @ORM\Table(name="articles")
+ * @ORM\Entity(repositoryClass="EhsBundle\Repository\ArticlesRepository")
+ */
+class Articles
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="titre", type="string", length=255)
+     */
+    private $titre;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="contenu", type="text")
+     */
+    private $contenu;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="chapeau", type="text")
+     */
+    private $chapeau;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set titre
+     *
+     * @param string $titre
+     *
+     * @return Articles
+     */
+    public function setTitre($titre)
+    {
+        $this->titre = $titre;
+
+        return $this;
+    }
+
+    /**
+     * Get titre
+     *
+     * @return string
+     */
+    public function getTitre()
+    {
+        return $this->titre;
+    }
+
+    /**
+     * Set contenu
+     *
+     * @param string $contenu
+     *
+     * @return Articles
+     */
+    public function setContenu($contenu)
+    {
+        $this->contenu = $contenu;
+
+        return $this;
+    }
+
+    /**
+     * Get contenu
+     *
+     * @return string
+     */
+    public function getContenu()
+    {
+        return $this->contenu;
+    }
+
+    /**
+     * Set chapeau
+     *
+     * @param string $chapeau
+     *
+     * @return Articles
+     */
+    public function setChapeau($chapeau)
+    {
+        $this->chapeau = $chapeau;
+
+        return $this;
+    }
+
+    /**
+     * Get chapeau
+     *
+     * @return string
+     */
+    public function getChapeau()
+    {
+        return $this->chapeau;
+    }
+}
+
