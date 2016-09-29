d056464277bea84e50f449e13e50679d392f0a2e
LaGvidilo <cyrilcoelho67@gmail.com>
2016-09-29 16:43:25 +0200

    Fiche contact
    Fiche contact qui envoi un mail

---
+++ src/EhsBundle/Form/ContactType.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace EhsBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Gregwar\CaptchaBundle\Type\CaptchaType;
+class ContactType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('nom','text',array('required'=>true,'attr'   =>  array('class'   => 'form-control')))
+            ->add('prenom','text',array('required'=>true,'attr'   =>  array('class'   => 'form-control')))
+            ->add('telephone','text',array('required'=>true,'attr'   =>  array('class'   => 'form-control')))
+            ->add('email','text',array('required'=>true,'attr'   =>  array('class'   => 'form-control')))
+            ->add('message','textarea',array('required'=>true,'attr'   =>  array('class'   => 'form-control')))
+            ->add('captcha', CaptchaType::class, array('reload'=>true, 'as_url'=> true, 'invalid_message'=>'Code incorrect!','attr'   =>  array('class'   => 'form-control')))
+            
+        ;
+    }
+    
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'EhsBundle\Entity\Contact'
+        ));
+    }
+}
