82316058f8f70db5da228b1c8cc46417cddbdf9d
Benjamin <benjamin.hacques@nantes.imie.fr>
2016-10-05 12:46:24 +0200

    archive done, composer update required

---
+++ src/EhsBundle/Entity/Archive.php
@@ -0,0 +1,160 @@
+<?php
+
+namespace EhsBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\HttpFoundation\File\File;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
+
+/**
+ * Archive
+ *
+ * @ORM\Table(name="archive")
+ * @ORM\Entity(repositoryClass="EhsBundle\Repository\ArchiveRepository")
+ * @Vich\Uploadable
+ */
+class Archive
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="documentFile", type="string", length=255)
+     */
+    private $documentName;
+
+    /**
+     * NOTE: This is not a mapped field of entity metadata, just a simple property.
+     * 
+     * @Vich\UploadableField(mapping="archive_document", fileNameProperty="documentName")
+     * 
+     * @var File
+     */
+    private $documentFile;
+
+
+    /**
+     * @ORM\Column(name="date", type="datetime")
+     *
+     * @var \DateTime
+     */
+    private $date;
+
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="titre", type="string", length=255)
+     */
+    private $titre;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="contenu", type="text")
+     */
+    private $text;
+
+    /**
+     * If manually uploading a file (i.e. not using Symfony Form) ensure an instance
+     * of 'UploadedFile' is injected into this setter to trigger the  update. If this
+     * bundle's configuration parameter 'inject_on_load' is set to 'true' this setter
+     * must be able to accept an instance of 'File' as the bundle will inject one here
+     * during Doctrine hydration.
+     *
+     * @param File|\Symfony\Component\HttpFoundation\File\UploadedFile $document
+     *
+     * @return Archive
+     */
+    public function setDocumentFile(File $document = null)
+    {
+        $this->documentFile = $document;
+
+        if ($document) {
+            // It is required that at least one field changes if you are using doctrine
+            // otherwise the event listeners won't be called and the file is lost
+            $this->date = new \DateTime();
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return File|null
+     */
+    public function getDocumentFile()
+    {
+        return $this->documentFile;
+    }
+
+
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set documentName
+     *
+     * @param string $documentName
+     *
+     * @return Archive
+     */
+    public function setDocumentName($documentName)
+    {
+        $this->documentName = $documentName;
+
+        return $this;
+    }
+
+    /**
+     * Get documentName
+     *
+     * @return string
+     */
+    public function getDocumentName()
+    {
+        return $this->documentName;
+    }
+
+    public function getDate() {
+        return $this->date;
+    }
+
+    public function setDate(\DateTime $date) {
+        $this->date = $date;
+    }
+        
+    public function getTitre() {
+        return $this->titre;
+    }
+
+    public function setTitre($titre) {
+        $this->titre = $titre;
+    }
+        
+    public function getText() {
+        return $this->text;
+    }
+
+    public function setText($text) {
+        $this->text = $text;
+    }
+        
+    }
+
